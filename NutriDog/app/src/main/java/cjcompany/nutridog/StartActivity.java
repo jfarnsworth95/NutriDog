b53a163698b3aa21fb52544c7e945ae25c7885ba
Jackson F <jafarns95@gmail.com>
2017-06-19 18:59:56 -0500

    finished testing

--- NutriDog/app/src/main/java/cjcompany/nutridog/StartActivity.java
+++ NutriDog/app/src/main/java/cjcompany/nutridog/StartActivity.java
@@ -3,6 +3,11 @@ package cjcompany.nutridog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//For help storing credentials for Git command line:
+//      $ git config credential.helper store
+//      $ git push http://example.com/repo.git
+
+
 public class StartActivity extends AppCompatActivity {
 
     @Override
