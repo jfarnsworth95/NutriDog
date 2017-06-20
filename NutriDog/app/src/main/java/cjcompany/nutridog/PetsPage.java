42e645aa377ac9d1b07f40b4c8c52686eeab7b48
Jackson F <jafarns95@gmail.com>
2017-06-20 13:26:42 -0500

    renamed StartActivity to StartPage, changed args in it as well. Filled out methods in PetsPage.

--- NutriDog/app/src/main/java/cjcompany/nutridog/PetsPage.java
+++ NutriDog/app/src/main/java/cjcompany/nutridog/PetsPage.java
@@ -12,5 +12,57 @@ public class PetsPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pets);
+
+        //if pets.csv exists, use createPetButtons
+
+        //if pets.csv does not exist, use newPetForm
+    }
+
+    public void createPetButtons(){
+        //find pets from sheet
+        //creates square buttons in two columns
+        //creates addNewPet button at bottom
+    }
+
+    public void newPetForm(){
+        //following textEdit fields:
+            //name
+            //breed
+            //height
+
+        //maybe allow input picture of dog? (limit resolution, save only whats req.)
+        //keep pictures for timeline?
+
+        //then submit button
+
+        //from breed, generate:
+            //envrionment? waiting on Cary for these.
+            //excersises?
     }
 }
+
+/*
+
+    ******  EXAMPLE CODE FOR XML INJECTION  ******
+
+    public void createTableView() {
+        //make all list elements buttons that change color when pressed
+        //insert totalPlayers variable
+        int totalPlayers = 0;
+
+        TableLayout tl = (TableLayout) findViewById(R.id.voteTable);
+
+        for (int i = 0; i < 10; i++) {
+            TableRow row = new TableRow(this);
+
+            for (int e = 0; e < totalPlayers; e++) {
+
+                CheckBox checkBox = new CheckBox(this);
+                checkBox.setText(acceptedAnswers.get(e));
+                row.addView(checkBox);
+            }
+
+            tl.addView(row);
+        }
+    }
+*/
--- NutriDog/app/src/main/java/cjcompany/nutridog/StartPage.java
+++ NutriDog/app/src/main/java/cjcompany/nutridog/StartPage.java
@@ -3,13 +3,14 @@ package cjcompany.nutridog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 //For help storing credentials for Git command line:
 //      $ git config credential.helper store
 //      $ git push http://example.com/repo.git
 
 
-public class StartActivity extends AppCompatActivity {
+public class StartPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,19 +18,19 @@ public class StartActivity extends AppCompatActivity {
         setContentView(R.layout.activity_start);
     }
 
-    public void goTo_PetsPage(){
+    public void goTo_PetsPage(View view){
         Intent intent = new Intent(this, PetsPage.class);
         //intent.putExtra(EXTRA_MESSAGE, androidId);
         startActivity(intent);
     }
 
-    public void goTo_VetsPage(){
+    public void goTo_VetsPage(View view){
         Intent intent = new Intent(this, VetsPage.class);
         //intent.putExtra(EXTRA_MESSAGE, androidId);
         startActivity(intent);
     }
 
-    public void goTo_ToysPage(){
+    public void goTo_ToysPage(View view){
         Intent intent = new Intent(this, ToysPage.class);
         //intent.putExtra(EXTRA_MESSAGE, androidId);
         startActivity(intent);
