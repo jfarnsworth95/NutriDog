c1a554890f9887b5ea5b5647df95a1841f769c3b
Jackson F <jafarns95@gmail.com>
2017-06-29 16:12:08 -0500

    added calorie counter on petInfoPage, minor additions to mealPage and activity

--- NutriDog/app/src/main/java/cjcompany/nutridog/MealPage.java
+++ NutriDog/app/src/main/java/cjcompany/nutridog/MealPage.java
@@ -1,7 +1,19 @@
 package cjcompany.nutridog;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Jackson on 6/29/2017.
  */
-public class MealPage {
+public class MealPage extends AppCompatActivity {
+
+
+    //TODO make sure info is saved when device moves away from app
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //TODO should be passed string var of "Breakfast", "Lunch", or "Dinner"
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meal);
+    }
 }
