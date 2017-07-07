fc520ca37e444a69e2588535089179522f792e10
Jackson F <jafarns95@gmail.com>
2017-07-07 11:16:00 -0500

    created search component for dog food file, now about to test

--- NutriDog/app/src/main/java/cjcompany/nutridog/SearchableActivity.java
+++ NutriDog/app/src/main/java/cjcompany/nutridog/SearchableActivity.java
@@ -6,6 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -27,11 +31,60 @@ public class SearchableActivity extends ListActivity {
     }
 
     public ArrayAdapter<String> findMatchingDogFood(String query){
+
+        String dogFoodFile = "cleanDogFood.csv";
+
         //when searching, cut off first index, which corresponds to brand
         ArrayList<String> matchingResults = new ArrayList<String>();
+        BufferedReader br = null;
 
+        try{
+            FileReader fr = new FileReader("/NutriDog/app/src/main/assets/" + dogFoodFile);
+            br = new BufferedReader(fr);
 
+            String str = br.readLine();
+            while (str != null){
 
-        return null; //TODO change to actual adapter handle
+                //split up file line being read
+                String[] theSplit = str.split(",");
+
+                String brandPrefix = theSplit[0] + " - ";
+                //if the first index contains the query, return the entire line
+                if (theSplit[0].contains(query)){
+                    for(int i = 1; i < theSplit.length; i ++){
+                        matchingResults.add(brandPrefix + theSplit[i]);
+                    }
+                //if element in list from file line contains query, add to arraylist
+                }else{
+                    for(int e = 1; e < theSplit.length; e ++){
+                        if(theSplit[e].contains(query)){
+                            matchingResults.add(brandPrefix + theSplit[e]);
+                        }
+                    }
+                }
+            }
+        }
+        catch (FileNotFoundException ex){
+            System.err.println("DogFood.csv is not at expected location");
+        }
+        catch (IOException ex1){
+            System.err.println("Error while reading file");
+        }
+        finally{
+            try{
+                if (br != null){
+                    br.close();
+                }
+            }
+            catch(Exception ex2){
+                ex2.printStackTrace();
+                System.exit(-1);
+            }
+        }
+
+        //move arraylist to arrayAdapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,matchingResults);
+        //return arrayAdapter
+        return adapter;
     }
 }
