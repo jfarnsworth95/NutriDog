5d31c5a6bcad278a1f588e04b30f851b9a28215a
Jackson F <jafarns95@gmail.com>
2017-07-20 18:39:14 -0500

    completed PetsPage.class, removed menus folder once it became obsolete[sic?]

--- NutriDog/app/src/main/java/cjcompany/nutridog/PetsPage.java
+++ NutriDog/app/src/main/java/cjcompany/nutridog/PetsPage.java
@@ -1,14 +1,20 @@
 package cjcompany.nutridog;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.app.AlertDialog.Builder;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.PopupMenu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.ScrollView;
 import android.widget.Toast;
 
@@ -20,6 +26,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Random;
 
 // Handle extra files with:
 // https://developer.android.com/guide/topics/data/data-storage.html#filesInternal
@@ -124,21 +131,23 @@ public class PetsPage extends AppCompatActivity {
         LinearLayout rightColumn = (LinearLayout) findViewById(R.id.pets_rightColumn);
 
         for(int even = 0; even < dogInfos.size(); even +=2){
-            Button newLeftButton = createButton(dogInfos.get(even)[0],new Integer(dogInfos.get(even)[1]));
+            Button newLeftButton = createButton(dogInfos.get(even)[0],dogInfos.get(even)[1]);
             leftColumn.addView(newLeftButton);
         }
         if(dogInfos.size() > 1){
             for(int odd = 1; odd < dogInfos.size(); odd += 2){
-                Button newRightButton = createButton(dogInfos.get(odd)[0],new Integer(dogInfos.get(odd)[1]));
+                Button newRightButton = createButton(dogInfos.get(odd)[0],dogInfos.get(odd)[1]);
                 rightColumn.addView(newRightButton);
             }
         }
     }
 
-    private Button createButton(final String name, final int uniqueID){
-        final Button btn = new Button(this);
+    private Button createButton(final String name, final String uniqueID){
+        //sets height, width, text size, and margin in R.id.btn_pet
+        final Button btn = (Button) findViewById(R.id.btn_pet);
         btn.setText(name);
 
+        //set button onClick and onLongClick methods
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -146,6 +155,7 @@ public class PetsPage extends AppCompatActivity {
                 //add pet reference to EXTRA_MESSAGE
                 //intent.putExtra(EXTRA_MESSAGE, androidId);
                 intent.putExtra("DOGS_NAME",name);
+                intent.putExtra("DOGS_ID",uniqueID);
                 startActivity(intent);
             }
         });
@@ -154,29 +164,38 @@ public class PetsPage extends AppCompatActivity {
         btn.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                PopupMenu popup = new PopupMenu(PetsPage.this, btn);
-                popup.getMenuInflater().inflate(R.menu.popup_pet_delete, popup.getMenu());
-                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                    public boolean onMenuItemClick(MenuItem item) {
-                        Toast.makeText(PetsPage.this,"Deleted data for " + btn.getText(),Toast.LENGTH_SHORT).show();
-
-                        //TODO Make confirmation window/popup
-                        deletePetInfo(name,uniqueID);
-                        return true;
-                    }
-                });
-                popup.show();//showing popup menu
+                //https://www.mkyong.com/android/android-alert-dialog-example/
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(PetsPage.this);
+                // set title
+                alertDialogBuilder.setTitle("Are you sure you want to remove pet data?");
+                // set dialog message
+                alertDialogBuilder
+                        .setCancelable(false)
+                        .setPositiveButton("Delete",new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                // if this button is clicked, delete pet info
+                                deletePetInfo(name,uniqueID);
+                                Toast.makeText(PetsPage.this,"Deleted data for " + btn.getText(),Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton("Cancel",new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                // if this button is clicked, just close the dialog box and do nothing
+                                dialog.cancel();
+                            }
+                        });
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                // show it
+                alertDialog.show();
                 return true;
             }
         });
 
-        //TODO set button appearence (ex: size, text size, padding, ...)
-        //TODO set button onClick and onLongClick methods
-
         return btn;
     }
 
-    private void deletePetInfo(String name, int uniqueID){
+    private void deletePetInfo(String name, String uniqueID){
         BufferedReader br = null;
         try {
 
@@ -185,12 +204,13 @@ public class PetsPage extends AppCompatActivity {
             br = new BufferedReader(new InputStreamReader(fis));
 
             FileOutputStream outputStream = openFileOutput("tempFile.csv", Context.MODE_PRIVATE);
-
             String str = br.readLine();
 
             while(str != null) {
 
-                if (str.split(",")[0] != name) {
+                if (str.split(",")[0].equals(name) && str.split(",")[1].equals(uniqueID)) {
+
+                }else{
                     outputStream.write(str.getBytes());
                 }
                 str = br.readLine();
@@ -228,28 +248,81 @@ public class PetsPage extends AppCompatActivity {
     public void onNewPetBtnClicked(View view){
         Intent i = new Intent(this, PetsPage.class);
         setContentView(R.layout.activity_pets_addnew);
+        Button btn = (Button) findViewById(R.id.btn_submitNewPet);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSubmitNewPet();
+            }
+        });
     }
 
-
-    //TODO CONNECT TO XML FILE
-    private void onSubmitNewPet(View view) {
-        Intent i = new Intent(this, PetsPage.class);
-        //TODO save dog info to pets.csv
-
-        //TODO create dog's info file (Named it: DogName_Data_UniqueID.csv)
-
-
-        //reload page
-        try {
+    private void onSubmitNewPet() {
+        //save dog info to pets.csv
+        //Name,UniqueID,Age,Weight,Height,Breed,Gender
+        String name;
+        String id;
+        try{
             FileInputStream fis = openFileInput("pets.csv");
             br = new BufferedReader(new InputStreamReader(fis));
+
+            FileOutputStream outputStream = openFileOutput("tempFile.csv", Context.MODE_PRIVATE);
             String str = br.readLine();
-            createPetButtons(str);
-        } catch (FileNotFoundException ex) {
-            System.err.println("PET FILE MISSING: REMOVED SINCE STARTUP");
-        } catch (IOException ex) {
-            System.err.println("UNEXPECTED IO EXCEPTION");
+
+            while(str != null) {
+                outputStream.write(str.getBytes());
+                str = br.readLine();
+            }
+
+            String newPetData = ""; //Name,UniqueID,Age,Weight,Height,Breed,Gender
+            EditText petName = (EditText) findViewById(R.id.petName);
+            newPetData += "," + petName.getText().toString();
+            name = petName.getText().toString();
+            Long petID = Math.abs(new Random().nextLong());
+            newPetData += "," + new Long(petID).toString();
+            id = new Long(petID).toString();
+            EditText petAge = (EditText) findViewById(R.id.petAge);
+            newPetData += "," + petAge.getText().toString();
+            EditText petWeight = (EditText) findViewById(R.id.petWeight);
+            newPetData += "," + petWeight.getText().toString();
+            EditText petHeight = (EditText) findViewById(R.id.petHeightInches);
+            newPetData += "," + petHeight.getText().toString();
+            EditText petBreed = (EditText) findViewById(R.id.petBreed);
+            newPetData += "," + petBreed.getText().toString();
+            RadioButton maleBtn = (RadioButton) findViewById(R.id.radioBtn_male);
+            if(maleBtn.isChecked()){
+                newPetData += ",Male";
+            }else{
+                newPetData += ",Female";
+            }
+            outputStream.write(newPetData.getBytes());
+
+            outputStream.close();
+
+            File oldFile = new File(getFilesDir(),"pets.cvs");
+            oldFile.delete();
+            File newFile = new File(this.getFilesDir(), "tempFile.csv");
+            newFile.renameTo(new File("pets.csv"));
+
+            //create dog's info file (Named it: DogName_Data_UniqueID.csv)
+            FileOutputStream fos = openFileOutput(name + "_Data_" + id + ".csv", Context.MODE_PRIVATE);
+            fos.write("".getBytes());
+            fos.close();
+
+        }catch(IOException ex){
+            System.err.println("UNEXPECTED ERROR");
+        }finally {
+            try {
+                if (br != null) {
+                    br.close();
+                }
+            } catch (IOException ex) {
+                System.err.println("UNEXPECTED ERROR");
+            }
         }
+
+        //reload page
+        createPetButtons();
     }
 
 }
@@ -278,4 +351,45 @@ public class PetsPage extends AppCompatActivity {
             tl.addView(row);
         }
     }
+
+
+       ***** POP-UP MENU IDEA *****
+
+    PopupMenu popup = new PopupMenu(PetsPage.this, btn);
+        popup.getMenuInflater().inflate(R.menu.popup_pet_delete, popup.getMenu());
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            public boolean onMenuItemClick(MenuItem item) {
+
+                //https://www.mkyong.com/android/android-alert-dialog-example/
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+                // set title
+                alertDialogBuilder.setTitle("Are you sure you want to remove pet data?");
+                // set dialog message
+                alertDialogBuilder
+                        .setCancelable(false)
+                        .setPositiveButton("Delete",new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                // if this button is clicked, delete pet info
+                                deletePetInfo(name,uniqueID);
+                                Toast.makeText(PetsPage.this,"Deleted data for " + btn.getText(),Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton("Cancel",new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                // if this button is clicked, just close the dialog box and do nothing
+                                dialog.cancel();
+                            }
+                        });
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                // show it
+                alertDialog.show();
+                return true;
+            }
+        });
+        popup.show();//showing popup menu
+        return true;
+    }
+
 */
+
